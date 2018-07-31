d5755ff23c4b6545659fc9808855909a7a185f53
xu.shuifeng <xushuifeng@xushuifengdeMacBook-Pro.local>
2018-07-31 16:17:46 +0800

    add headerview

---
+++ V2EX/Application/Timeline/TimelineHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  TimelineHeaderView.swift
+//  V2EX
+//
+//  Created by xu.shuifeng on 2018/7/31.
+//  Copyright © 2018 shuifeng.me. All rights reserved.
+//
+
+import UIKit
+
+class TimelineHeaderView: UIView {
+    
+}
